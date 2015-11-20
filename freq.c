fcdc1a631b309f4b1789f965335854f51a36025c
mitul227 <mituljoshi227@gmail.com>
2015-11-20 19:20:09 +0530

    Frequency calculator
    The program takes input as words from a file and then prints the words
    along with their frequency in descending order in a file named output
    ,i.e, the most used word is printed first and then so on

--- freq.c
+++ freq.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
-struct s           //an array of structure is defined to store each word of the file
+struct words           //an array of structure is defined to store each word of the file
 {
     char a[20];
     int count;
-}s[100000];
+}words[100000];
 void main()
 {
-    FILE *f=NULL;
+    FILE *f=NULL,*f2=NULL;
     int i=0,j=0,k,l,max;
-    char ch,b[20];
-    f=fopen("xyz.txt","r");
+    char b[20],filename[20],ch;
+    printf("Enter the filename\n");
+    scanf("%s",filename);
+    f=fopen(filename,"r");
+    f2=fopen("output.txt","w");
     if(f==NULL)
     {
         printf("File doesn't exist\n");
@@ -17,37 +20,44 @@ void main()
     }
     else
     {
-        while(fscanf(f,"%s",b)!=EOF)
+        ch=tolower(fgetc(f));  //tolower is used to convert every character to a lowercase letter
+        while(ch!=EOF)
         {
-            l=strlen(b);
-            for(j=0;j<l;j++)
-            b[j]=tolower(b[j]);
-            strcpy(s[i].a,b);
+            while(ch!=' '&&ch!='.'&&ch!=','&&ch!='!'&&ch!='('&&ch!=')'&&ch!=';'&&ch!=':'&&ch!='!'&&ch!='?'&&ch!='"'&&ch!=39&&ch!='\n'&&ch!=EOF)
+            {
+                words[i].a[j++]=ch;
+                ch=tolower(fgetc(f));
+            }
+            words[i].a[j]='\0';
             i++;
+            j=0;
+            ch=tolower(fgetc(f));
         }
     }
     for(j=0;j<i;j++)
     {
-        if(strcmp(s[j].a,"\0")!=0)
+        if(strcmp(words[j].a,"\0")!=0)
         {
-            s[j].count=1;
+            words[j].count=1;
             for(k=j+1;k<i;k++)
             {
-                if(strcmp(s[j].a,s[k].a)==0)
+                if(strcmp(words[j].a,words[k].a)==0)
                 {
-                    strcpy(s[k].a,"\0");
-                    s[j].count++;
+                    strcpy(words[k].a,"\0");    //Assigning value of '\0' eleminates the repetition of each word
+                    words[j].count++;
                 }
             }
         }
     }
+    fprintf(f2,"               Word\t-\tFrequency\n");
+    fprintf(f2,"               ----\t \t---------\n\n");
     for(k=0;k<i;k++)
     {
-        if(strcmp(s[k].a,"\0")!=0)
+        if(strcmp(words[k].a,"\0")!=0)
         {
-            for(l=0;l<i;l++)
-            {
-                if(s[l].count!=0)
+            for(l=0;l<i;l++)             //This loop finds the first word whose count is not zero
+            {                             //and assigns the value of it's index to max
+                if(words[l].count!=0)
                 {
                     max=l;
                     break;
@@ -55,14 +65,15 @@ void main()
             }
             for(j=0;j<i;j++)
             {
-                if(s[j].count>s[max].count)
+                if(words[j].count>words[max].count)
                 max=j;
             }
-            printf("%20s\t-\t%d\n",s[max].a,s[max].count);
-            s[max].count=0;
+            fprintf(f2,"%20s\t-\t%d\n",words[max].a,words[max].count);
+            words[max].count=0;
         }
     }
 }
+
 
 
 
